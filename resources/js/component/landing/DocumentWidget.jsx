05e2db32de9f335b31e548e1390c524a34f08494
Vincent <vincentferrer1010@gmail.com>
2025-03-03 13:47:42 +0800

    widgettransitionlandingpage

--- resources/js/component/landing/DocumentWidget.jsx
+++ resources/js/component/landing/DocumentWidget.jsx
@@ -1,16 +1,23 @@
-// DocumentWidget.jsx
 import React from 'react';
 import '../../../css/styles/landing/DocumentWidget.css';
 
 const DocumentWidget = () => {
+    const handleClick = () => {
+        // You can implement any action on click if needed
+    };
+
     return (
-        <div className="widget">
+        <div className="widget" onClick={handleClick}>
             <div className="widget-content">
-                <img src="/images/documentcontrol.png" alt="Document Control" className="logo" />
+                <img 
+                    src="/images/documentcontrol.png" 
+                    alt="Document Control" 
+                    className="logo" 
+                />
                 <div className="widget-header">
                     <div className="widget-name">Document Control</div>
                     <div className="widget-text">
-                    A systematic approach to managing, storing, and tracking documents to ensure accuracy, accessibility, and compliance within an organization.
+                        A systematic approach to managing, storing, and tracking documents to ensure accuracy, accessibility, and compliance within an organization.
                     </div>
                 </div>
             </div>
