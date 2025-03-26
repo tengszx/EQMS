d760de324390353c1358dc87866298e3f61effc0
Teng <vincentferrer1010@gmail.com>
2025-03-26 19:51:33 +0800

    see you tom

--- resources/js/component/landing/Startup.jsx
+++ resources/js/component/landing/Startup.jsx
@@ -1,28 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import '../../../css/styles/landing/Startup.css';
 
 const Startup = () => {
-    useEffect(() => {
-        console.log("Startup component mounted");
-        return () => {
-            console.log("Startup component unmounted");
-        };
-    }, []);
-
     return (
-        <div className="startup-container" style={{ backgroundImage: `url(/images/startupbackground.png)` }}>
-            {/* Content wrapper with enhanced text readability */}
+        <div className="startup-container">
             <div className="content-wrapper">
-                <h1 className="main-title">
-                    {"QualityManagementSystem".split("").map((letter, index) => (
-                        <span key={index} className="bubble-letter">
-                            {letter}
-                        </span>
-                    ))}
-                </h1>
-                <h2 className="sub-title">Ensure excellence in process and quality</h2>
+                <h1 className="main-title">Quality Management System</h1>
+                <h2 className="sub-title">Ensure excellence in processes and quality</h2>
                 <p className="footer-text">NATIONAL RESEARCH COUNCIL OF THE PHILIPPINES</p>
-                <button className="learn-more-button">Learn More</button>
+                <button className="get-started-button">Get Started</button>
             </div>
         </div>
     );
